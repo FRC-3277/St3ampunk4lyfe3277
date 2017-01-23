6f8c04930bc89e5fa9173335ee12866d2163fd2c
FRC-3277 <prodigi3277@gmail.com>
2017-01-23 17:48:55 -0600

    Initialization of Repository

---
+++ src/Subsystems/DriveTrain.h
@@ -0,0 +1,18 @@
+#ifndef DRIVETRAIN_H
+#define DRIVETRAIN_H
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+
+class DriveTrain: public Subsystem {
+private:
+	std::shared_ptr<Motor> leftMotor;
+	std::shared_ptr<CANTalon> rightMotor;
+	std::shared_ptr<RobotDrive> robotDrive21;
+public:
+	DriveTrain();
+	void InitDefaultCommand();
+
+	void setdrive(float speedleft, float speedright);
+	void openmotors();
+
+#endif
