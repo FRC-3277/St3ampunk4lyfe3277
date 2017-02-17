b7b83e6124e4c939189feb243ebbef8e7e6efc2c
FRC-3277 <prodigi3277@gmail.com>
2017-02-16 20:06:02 -0600

    Still not working...

--- src/Subsystems/InTheRearWithTheGearLidar.cpp
+++ src/Subsystems/InTheRearWithTheGearLidar.cpp
@@ -17,30 +17,37 @@ void InTheRearWithTheGearLidar::InitDefaultCommand() {
 // Update distance variable. See the Quick Start Guide for documentation
 int InTheRearWithTheGearLidar::updateDistance()
 {
-	int loVal = 0, hiVal = 0;
-	lumberJack->dLog("updateDistance1");
-	i2c->Write(LIDAR_CONFIG_REGISTER, (uint8_t)0x04); // Initiate measurement
-	//this_thread::sleep_for(chrono::milliseconds(40)); // Delay for measurement to be taken
-	Sleep(40);
-	lumberJack->dLog("updateDistance1_1");
-	//i2c->Read(LIDAR_DISTANCE_REGISTER, 2, &distance); // Read in
-	lumberJack->dLog("updateDistance2");
-	i2c->Read(LO_DISTANCE_REGISTER, 1, (uint8_t*)&loVal);
-	//this_thread::sleep_for(chrono::milliseconds(10)); // Delay to prevent over polling
-	Sleep(10);
-	lumberJack->dLog("updateDistance3");
-	i2c->Read(HIGH_DISTANCE_REGISTER, 1, (uint8_t*)&loVal);
-	//this_thread::sleep_for(chrono::milliseconds(10)); // Delay to prevent over polling
-	Sleep(10);
-
-	lumberJack->dLog("updateDistance4");
-	if(loVal == -1 || hiVal == -1)
+	try
 	{
-		lumberJack->eLog("Lidar distance failure");
+		int loVal = 0, hiVal = 0;
+		lumberJack->dLog("updateDistance1");
+		i2c->Write(LIDAR_CONFIG_REGISTER, (uint8_t)0x04); // Initiate measurement
+		//this_thread::sleep_for(chrono::milliseconds(40)); // Delay for measurement to be taken
+		//Sleep(40);
+		lumberJack->dLog("updateDistance1_1");
+		//i2c->Read(LIDAR_DISTANCE_REGISTER, 2, &distance); // Read in
+		lumberJack->dLog("updateDistance2");
+		i2c->Read(LO_DISTANCE_REGISTER, 1, (uint8_t*)&loVal);
+		//this_thread::sleep_for(chrono::milliseconds(10)); // Delay to prevent over polling
+		//Sleep(10);
+		lumberJack->dLog("updateDistance3");
+		i2c->Read(HIGH_DISTANCE_REGISTER, 1, (uint8_t*)&loVal);
+		//this_thread::sleep_for(chrono::milliseconds(10)); // Delay to prevent over polling
+		//Sleep(10);
+
+		lumberJack->dLog("updateDistance4");
+		if(loVal == -1 || hiVal == -1)
+		{
+			lumberJack->eLog("Lidar distance failure");
+		}
+		lumberJack->dLog("updateDistance5");
+		distance = (hiVal << 8) + loVal;
+		lumberJack->dLog("updateDistance6");
 	}
-	lumberJack->dLog("updateDistance5");
-	distance = (hiVal << 8) + loVal;
-	lumberJack->dLog("updateDistance6");
+	catch (exception& e)
+	{
+		lumberJack->eLog(e.what());
+	}
 
 	return distance;
 }
@@ -53,8 +60,9 @@ void InTheRearWithTheGearLidar::neverEndingUpdater()
 		try
 		{
 			distance = updateDistance();
+			lumberJack->dLog("Distance: " + to_string(distance));
 			//this_thread::sleep_for(chrono::milliseconds(10));
-			Sleep(10);
+			//Sleep(10);
 		}
 		catch (exception& e)
 		{
