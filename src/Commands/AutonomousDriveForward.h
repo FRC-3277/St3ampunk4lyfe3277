cac7759a4cfa4fc62f8ebcfb87d53992a769e93e
FRC-3277 <prodigi3277@gmail.com>
2017-02-18 14:27:33 -0600

    Autonomous Drive Ahead commands

---
+++ src/Commands/AutonomousDriveForward.h
@@ -0,0 +1,22 @@
+#ifndef AutonomousDriveForward_H
+#define AutonomousDriveForward_H
+
+#include "Commands/Subsystem.h"
+#include "../Robot.h"
+
+
+class AutonomousDriveForward : public Command {
+public:
+	AutonomousDriveForward();
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+
+private:
+	double StarboardRevolutions;
+	double PortRevolutions;
+};
+
+#endif  // AutonomousDriveForward_H
